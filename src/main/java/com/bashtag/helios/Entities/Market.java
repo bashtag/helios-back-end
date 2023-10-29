118c2166591752820948f38fe977cc59b6fca6fd
bashtag <btstsiy@hotmail.com>
2023-10-29 21:01:27 +0300

    market model

--- src/main/java/com/bashtag/helios/Entities/Market.java
+++ src/main/java/com/bashtag/helios/Entities/Market.java
@@ -13,46 +13,27 @@ public class Market {
 	@Id
 	private ObjectId	_id;
 
-	// market id's
-	private Integer	id;
-
 	private String	marketName;
 	private String	seoUrl;
 
-	public Market() {
-	}
-
-	public Market(Integer id, String marketName, String seoUrl) {
-		this.id = id;
-		this.marketName = marketName;
-		this.seoUrl = seoUrl;
-	}
-
+	// getters and setters
 	public ObjectId get_id() {
 		return _id;
 	}
-
-	public Integer getId() {
-		return id;
+	public void set_id(ObjectId _id) {
+		this._id = _id;
 	}
-
 	public String getMarketName() {
 		return marketName;
 	}
-
-	public String getSeoUrl() {
-		return seoUrl;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
 	public void setMarketName(String marketName) {
 		this.marketName = marketName;
 	}
-
+	public String getSeoUrl() {
+		return seoUrl;
+	}
 	public void setSeoUrl(String seoUrl) {
 		this.seoUrl = seoUrl;
 	}
+
 }
