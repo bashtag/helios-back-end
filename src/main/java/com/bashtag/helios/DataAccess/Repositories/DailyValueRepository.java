29ad58bddb90627abe98ec818a59466a21c54229
bashtag <btstsiy@hotmail.com>
2023-10-30 11:49:51 +0300

    repository to access data for daily values

---
+++ src/main/java/com/bashtag/helios/DataAccess/Repositories/DailyValueRepository.java
@@ -0,0 +1,11 @@
+package com.bashtag.helios.DataAccess.Repositories;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.bashtag.helios.DataAccess.Interfaces.IDailyValueDal;
+import com.bashtag.helios.Entities.DailyValue;
+
+public interface DailyValueRepository extends MongoRepository<DailyValue, ObjectId>, IDailyValueDal {
+	
+}
