aea1b51510168fbb3f1a75545664b53f30eedfd2
bashtag <btstsiy@hotmail.com>
2023-10-30 13:56:16 +0300

    repository to access the stock datas from db

--- src/main/java/com/bashtag/helios/DataAccess/Repositories/StockRepository.java
+++ src/main/java/com/bashtag/helios/DataAccess/Repositories/StockRepository.java
@@ -3,9 +3,14 @@ package com.bashtag.helios.DataAccess.Repositories;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.bashtag.helios.DataAccess.Interfaces.IStockDal;
 import com.bashtag.helios.Entities.Stock;
+import java.util.List;
 
-public interface StockRepository extends MongoRepository<Stock, ObjectId>, IStockDal {
-	
+
+public interface StockRepository extends MongoRepository<Stock, ObjectId> {
+
+	List<Stock> findBySymbol(String symbol);
+
+	List<Stock> findByMarket(ObjectId market);
+
 }
