240dbb46c777c6fdcb853c2e73017bea95994b43
bashtag <btstsiy@hotmail.com>
2023-10-30 13:55:59 +0300

    repository for market data accessing from db

--- src/main/java/com/bashtag/helios/DataAccess/Repositories/MarketRepository.java
+++ src/main/java/com/bashtag/helios/DataAccess/Repositories/MarketRepository.java
@@ -3,9 +3,14 @@ package com.bashtag.helios.DataAccess.Repositories;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.bashtag.helios.DataAccess.Interfaces.IMarketDal;
 import com.bashtag.helios.Entities.Market;
+import java.util.List;
 
-public interface MarketRepository extends MongoRepository<Market, ObjectId>, IMarketDal {
-	
+// market collection is readonly so the queries too.
+public interface MarketRepository extends MongoRepository<Market, ObjectId> {
+	// find by id query
+	List<Market> findBy_id(ObjectId _id);
+
+	// find by market name
+	List<Market> findByMarketName(String marketName);
 }
